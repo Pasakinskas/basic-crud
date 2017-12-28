aad613f0777ad00efb65e008b9ad6f5c6ee72662
Marius Pasakinskas <marius.pasakinskas@gmail.com>
2017-12-28 22:50:59 +0200

    Last commit of the day. Tweaks and the post method

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.listen(3000, function() {
     console.log('listening on 3000');
 });
 
-console.log('i like to node it node it');
+app.get('/',(req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post('/quotes', (req, res) => {
+    console.log('I have received information!');
+    console.log(req.body);
+  })
