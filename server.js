70b967c72a40b1203742189c649caafaf594ebca
marius <marius.pasakinskas@gmail.com>
2018-01-27 23:00:00 +0200

    completed half of basic crud tutorial

--- server.js
+++ server.js
@@ -1,18 +1,34 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const MongoClient = require('mongodb').MongoClient
+const mongoLink = 'mongodb://Droztukas1:mmm03240742@ds123371.mlab.com:23371/crud-tutorial';
+let db;
 
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.listen(3000, function() {
-    console.log('listening on 3000');
+MongoClient.connect(mongoLink, (error, database) => {
+    if (error) {
+        return console.log(error);
+    }
+    db = database.db('crud-tutorial');
+    app.listen(3000, () => {
+        console.log('listening on 3000');
+    });
 });
 
 app.get('/',(req, res) => {
-    res.sendFile(__dirname + "/index.html");
+        db.collection('crud-example').find().toArray((err, result) => {
+        if (err) return console.log(err);
+        res.render('index.ejs', {quotes: result})
+    });
 });
 
 app.post('/quotes', (req, res) => {
-    console.log('I have received information!');
-    console.log(req.body);
-  })
+    db.collection('crud-example').save(req.body, (err, result) => {
+        if (err) return console.log(err);
+        console.log('Saved successfuly');
+        res.redirect('/');
+    });
+  });
