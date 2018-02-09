735214ede1650d38c77a651c281230bb99b52bd9
Marius Pasakinskas <marius.pasakinskas@gmail.com>
2018-02-09 23:11:19 +0200

    added form error. Update does not reset page.

--- public/main.js
+++ public/main.js
@@ -1,21 +1,49 @@
-let updateButton = document.getElementById('update');
-let deleteItemButton = document.getElementsByClassName('btn-delete');
+const updateButton = document.getElementById('update');
+const deleteItemButton = document.getElementsByClassName('btn-delete');
+const textAreas = document.getElementsByClassName('my-textbox');
 
-updateButton.addEventListener('click', () => {
+function addUpdateListener(button) {
+  button.addEventListener('click', () => {
     fetch('quotes', {
-        method: 'put',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({
-          name: 'Hobby',
-          quote: ' is too fun to be on the list',
-        })
-      }).then(res => {
-        if (res.ok) return res.json()
-      }).then(data => {
-        console.log(data)
-        window.location.reload(true)
+      method: 'put',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        name: 'Hobby',
+        quote: ' is too fun to be on the list',
+      }),
+    }).then(res => {
+      if (res.ok) return res.json()
+    }).then(data => {
+      const spanList = document.querySelectorAll('li.data-li span');
+      for (const span of spanList) {
+        if (span.innerText === "cook") {
+          span.innerText = "Hobby"; // cant find classes todo-name/body if I parentnode.parentnode
+          let cookBody = span.parentNode.parentNode.firstChild.nextSibling.nextSibling.firstChild;
+          cookBody.innerText = "is too fun to be on this list";
+          break;
+        }
+      }
     })
-});
+  });
+}
+
+addUpdateListener(updateButton);
+
+// updateButton.addEventListener('click', () => {
+//     fetch('quotes', {
+//         method: 'put',
+//         headers: {'Content-Type': 'application/json'},
+//         body: JSON.stringify({
+//           name: 'Hobby',
+//           quote: ' is too fun to be on the list',
+//         })
+//       }).then(res => {
+//         if (res.ok) return res.json()
+//       }).then(data => {
+//         console.log(data)
+//         window.location.reload(true)
+//     })
+// });
 
 document.querySelector('#saveQuote').addEventListener('submit', async event => {
   event.preventDefault();
@@ -30,7 +58,7 @@ document.querySelector('#saveQuote').addEventListener('submit', async event => {
     body: JSON.stringify(body),
   });
   form.reset(); // Deletes the text, that I've written into my form element
-  const res = await blob.json(); //Get blob, send back blob? What?
+  const res = await blob.json(); //Get blob, send back blob.json? What?
   const newQuote = document.createElement('li');
   newQuote.classList.add('row', 'data-li');
   newQuote.innerHTML = `<div class="col-xs-3"><span>${res.data.name}</span></div>
@@ -39,6 +67,16 @@ document.querySelector('#saveQuote').addEventListener('submit', async event => {
   document.querySelector('#quotesList').appendChild(newQuote);
   addDeleteListener(newQuote.querySelector('button'));
 });
+
+function addUppercaseListener(textbox) {
+  textbox.addEventListener('keypress', (event) => {
+    if(!event.key.match(/[0-9-a-z-\s-!?,]/)) {
+      event.preventDefault();
+      textbox.classList.add('warning');
+      textbox.parentNode.querySelector('#explain-form-error').classList.remove('on-demand');
+    }
+  });
+}
 
 function addDeleteListener (button) {
   button.addEventListener('click', () => {
@@ -56,4 +94,8 @@ function addDeleteListener (button) {
 
 for (const item of deleteItemButton) {
   addDeleteListener(item);
+};
+
+for (const item of textAreas) {
+  addUppercaseListener(item);
 };
